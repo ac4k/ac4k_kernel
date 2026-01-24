5ad7ac2c3570b342d84e9186195642be0b5aec93
Jian Wu <jianwuqaq@163.com>
2026-01-25 03:15:58 +0800

    Sparse linear attn (#18)
    * Add sparse linear attn

--- python/ac4k_kernel/ops/__init__.py
+++ python/ac4k_kernel/ops/__init__.py
@@ -2,5 +2,9 @@ from .dot_scale import dot_scale
 from .quant import quantize
 from .attention import attention
 from .rope_3d import rope_3d_apply
+from .sparse_linear_attention import SparseLinearAttention
 
-__all__ = ["attention", "quantize", "rope_3d_apply", "dot_scale"]
+__all__ = [
+    "attention", "SparseLinearAttention", "quantize", "rope_3d_apply",
+    "dot_scale"
+]
